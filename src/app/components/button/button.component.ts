c4a25eafebbf87cd79576a6442a3b774737b6652
krushj <krushna@equitydatascience.com>
2024-10-20 14:30:41 +0530

    Button initial setup

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output } from '@angular/core';
+import { IconComponent } from '@componenets/icon/icon.component';
+import { Style } from '@interface/style';
+
 
 @Component({
-  selector: 'app-button',
+  selector: 'Button',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
+  @Input()
+  title : string = '';
 
+  @Input()
+  style : Style = {};
+
+  @Input()
+  disabled : boolean = false;
+
+  @Input()
+  buttonType : string = 'primary'
+
+  @Input()
+  buttonSize : string = 'medium'
+
+  @Input()
+  color : string = 'white'
+
+  buttonClass : string = '';
+
+  ngOnInit(): void {
+    this.buttonClass = `gama-${this.color}-${this.buttonType}-${this.buttonSize}-button`
+  }
+
+  @Output() onClick = new EventEmitter<MouseEvent>();
 }
