ce1725a83f4ad59d7de69f5cdae16b375e90f149
krushj <krushna@equitydatascience.com>
2024-11-09 14:26:02 +0530

    input view v1

--- src/app/view/input-view/input-view.component.ts
+++ src/app/view/input-view/input-view.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ButtonComponent, InputComponent } from '@gama/components';
+import { ButtonComponent, IconComponent, InputComponent } from '@gama/components';
+import { KeyValue, Snippet } from '@gama/interface';
+import { CopySnippetService } from '@gama/services';
 
 @Component({
   selector: 'input-view',
   standalone: true,
-  imports: [InputComponent, ButtonComponent, FormsModule],
+  imports: [InputComponent, ButtonComponent, FormsModule, IconComponent],
   templateUrl: './input-view.component.html',
 })
 export class InputViewComponent {
-  title: string = ''
+  themes: any[] = ['', 'info', 'success', 'warn', 'alert', 'disabled'];
+  borders: any[] = ['primary', 'secondary'];
+  labels : boolean[] = [false, true];
+
+  constructor(private cdr: ChangeDetectorRef, private copySnippetService: CopySnippetService) {
+  }
+  copySnippet(event: Event, params: KeyValue) {
+    const target = event.target as HTMLElement;
+    if (target.innerText !== 'check_circle') {
+      const tagSnippet = this._generateOnTheFlySnippet(params['label'] as boolean, params['theme'] as string);
+      this.copySnippetService.copyContent(tagSnippet, params);
+      target.innerText = 'check_circle';
+      target.style.color = '#00A699';
+      setTimeout(() => {
+        target.innerText = 'content_copy';
+        target.style.color = ''
+      }, 2000);
+      this.cdr.detectChanges();
+    }
+  }
+
+  _generateOnTheFlySnippet (label: boolean, theme: string) :string {
+    let tag = `<GInput${label ? ` label="Label" `: ''} placeholder="placeholder" border=":border"${(theme !== '' && theme !== 'disabled') ? ` theme=":theme"`: ''}${theme === 'disabled' ? ` [disabled]=[true]`: ''}><GButton [style]="{background : 'transparent'; border: 'none'}" iconButton size="small" iconName="search" ${label ? 'suffix': 'prefix'}/><span ${label ? 'error': 'hint'}>${label ? 'error': 'hint'}</span></GInput>`
+    return tag;
+  }
 }
